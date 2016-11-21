ba0f1a31c5cf2ee022b0e86cf842792c13ae4f74
Liz Tiller <elizabeth.tiller@gmail.com>
2016-11-21 15:53:52 -0500

    finally got hsla numbers to show up

--- public/main.js
+++ public/main.js
@@ -1,6 +1,19 @@
+let hue = 128
+let sat = 100
+let light = 50
+let alpha = 1
+
+//hsl (128,100 %, 50 %)
+const getHSL = () => {
+  return `hsla(${hue}, ${sat}%, ${light}%, ${alpha})`
+}
+
 const main = () => {
-  document.getElementById('root').textContent = 'Hello, World!'
+  const hueInput = document.querySelector('input[name=hue]')
+  hueInput.addEventListener('input', () => {
+    hue = hueInput.value
+    console.log(getHSL())
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
